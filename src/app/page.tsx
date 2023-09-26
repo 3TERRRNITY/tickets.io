4a7c57ac007b84de554b5e2faf5780837c8e7bb1
3TERRRNITY <qertisww@gmail.com>
2023-09-26 18:20:09 +0300

    add timer and responsive spans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { assets } from "../utils/asset-utils";
+import { assets } from "@/utils/asset-utils";
 import { type Framework, frameworks } from "../utils/framework-utils";
+import { Poppins } from "next/font/google";
 import { useState, useEffect } from "react";
-import { cn } from "../utils/tailwind-utils";
+import { cn } from "@/utils/tailwind-utils";
+import { FrameworkRotation } from "@/components/framework-rotation";
+import { CountdownTimer } from "../components/countdown-timer";
+
+const poppins = Poppins({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   const [currentFramework, setCurrentFramework] = useState<Framework>(
     frameworks[0]
   );
+  const [showBackground, setShowBackground] = useState(false);
 
   useEffect(() => {
     let currentIndex = 0;
@@ -19,8 +28,12 @@ export default function Home() {
       currentIndex = (currentIndex + 1) % frameworks.length;
     };
 
-    const intervalId = setInterval(rotateFrameworks, 200);
+    const intervalId = setInterval(rotateFrameworks, 2000);
     return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    setShowBackground(true);
   }, []);
   return (
     <main>
@@ -40,6 +53,7 @@ export default function Home() {
           }
         )}
       />
+
       <Image
         width={1200}
         height={1200}
@@ -54,7 +68,88 @@ export default function Home() {
           backgroundImage: `url(${assets.square})`,
           backgroundSize: "30px",
         }}
-      ></div>
+      />
+      <div
+        className={cn(
+          "bg-black fixed inset-0 transition-opacity duration-[1500ms]",
+          !showBackground ? "opacity-100" : "opacity-0"
+        )}
+      />
+      <div className="max-w-7xl mt-20 mx-auto">
+        <div className="flex flex-col items-center relative z-10">
+          <h1
+            className={`text-7xl max-w-3xl text-center leading-snug mb-12 ${poppins.className}`}
+          >
+            <Image
+              alt="Figma logo"
+              className="inline-block mr-8 -mt-2"
+              src={assets.figma}
+              height="50"
+              width="50"
+            />
+            to <FrameworkRotation currentFramework={currentFramework} /> will{" "}
+            <span
+              className={cn("transition-colors duration-200", {
+                "text-purple-300": currentFramework === "qwik",
+                "text-sky-300": currentFramework === "safari",
+                "text-yellow-300": currentFramework === "chrome",
+                "text-teal-300": currentFramework === "tailwind",
+                "text-blue-300": currentFramework === "react",
+                "text-green-300": currentFramework === "vue",
+                "text-orange-300": currentFramework === "svelte",
+                "text-red-300": currentFramework === "mobile",
+                "text-neutral-300": currentFramework === "desktop",
+              })}
+            >
+              never
+            </span>{" "}
+            be the same again
+          </h1>
+
+          <p className="mb-8">
+            <span className="text-gray-300">
+              Join us for an AI launch event by
+            </span>
+            <Image
+              height={20}
+              width={100}
+              src={assets.builder}
+              className="inline-block ml-1 "
+              alt="Builder logo"
+            />
+            +
+            <Image
+              height={20}
+              width={55}
+              src={assets.figmatwo}
+              alt="Figma logo"
+              className="inline-block mx-1"
+            />
+          </p>
+
+          <div className="mb-8">
+            <button
+              className={cn(
+                "text-black px-6 py-3 rounded-md text-sm font-semibold transition-colors duration-200",
+                {
+                  "bg-purple-300": currentFramework === "qwik",
+                  "bg-sky-300": currentFramework === "safari",
+                  "bg-yellow-300": currentFramework === "chrome",
+                  "bg-teal-300": currentFramework === "tailwind",
+                  "bg-blue-300": currentFramework === "react",
+                  "bg-green-300": currentFramework === "vue",
+                  "bg-orange-300": currentFramework === "svelte",
+                  "bg-red-300": currentFramework === "mobile",
+                  "bg-neutral-300": currentFramework === "desktop",
+                }
+              )}
+            >
+              Claim Ticket
+            </button>
+          </div>
+          <CountdownTimer currentFramework={currentFramework} />
+        </div>
+      </div>
     </main>
   );
 }
